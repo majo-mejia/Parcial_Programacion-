8ec866d5ddb864e52c3186a1c0f4031cb0088b6d
majo mejia <mariaj.mejiag2@uqvirtual.edu.co>
2026-09-24 21:25:58 -0500

    clase plan personalizado

--- src/main/java/co/edu/uniquindio/poo/parcial/model/PlanPersonalizado.java
+++ src/main/java/co/edu/uniquindio/poo/parcial/model/PlanPersonalizado.java
@@ -1,4 +1,25 @@
 package co.edu.uniquindio.poo.parcial.model;
 
-public class PlanPersonalizado {
+public class PlanPersonalizado extends PlanEntrenamiento {
+    private int cantidadSesiones;
+    private TipoEspecialidad especialidad;
+    private String objetivo;
+
+    public PlanPersonalizado(String codigo, String nombre, String descripcion, int duracionMeses,
+                             double valorMensual, TipoEstado estado, int cantidadSesiones,
+                             TipoEspecialidad especialidad, String objetivo) {
+        super(codigo, nombre, descripcion, duracionMeses, valorMensual, estado, TipoPlan.PERSONALIZADO);
+        this.cantidadSesiones = cantidadSesiones;
+        this.especialidad = especialidad;
+        this.objetivo = objetivo;
+    }
+
+    public int getCantidadSesiones() { return cantidadSesiones; }
+    public TipoEspecialidad getEspecialidad() { return especialidad; }
+    public String getObjetivo() { return objetivo; }
+
+    @Override
+    public double calcularValor() {
+        return getValorMensual() * getDuracionMeses();
+    }
 }
