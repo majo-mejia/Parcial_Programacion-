cef7a0fbbde2f1d528cee1dd0a4ce154e9b57711
jhzz-8 <juanj.juradoc@uqvirtual.edu.co>
2026-09-24 23:38:16 -0500

    mis clases

--- src/main/java/co/edu/uniquindio/poo/parcial/model/Cliente.java
+++ src/main/java/co/edu/uniquindio/poo/parcial/model/Cliente.java
@@ -1,4 +1,82 @@
 package co.edu.uniquindio.poo.parcial.model;
 
-public class Cliente {
+import java.time.LocalDate;
+
+public class Cliente implements Nombrable {
+
+    private String nombre;
+    private String documento;
+    private String telefono;
+    private String correoElectronico;
+    private int edad;
+    private LocalDate fechaRegistro;
+
+    public Cliente(String nombre, String documento, String telefono,
+                   String correoElectronico, int edad, LocalDate fechaRegistro) {
+        this.nombre = nombre;
+        this.documento = documento;
+        this.telefono = telefono;
+        this.correoElectronico = correoElectronico;
+        this.edad = edad;
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public boolean evaluarNumeroPerfecto() {
+        return NumeroPerfectoUtil.esNumeroPerfecto(telefono);
+    }
+
+    @Override
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public LocalDate getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(LocalDate fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " (" + documento + ")";
+    }
 }
--- src/main/java/co/edu/uniquindio/poo/parcial/model/ServicioAdicional.java
+++ src/main/java/co/edu/uniquindio/poo/parcial/model/ServicioAdicional.java
@@ -1,4 +1,86 @@
 package co.edu.uniquindio.poo.parcial.model;
 
-public class ServicioAdicional {
+public class ServicioAdicional implements Nombrable, Calculable {
+
+    private TipoServicio servicio;
+    private String codigo;
+    private String nombre;
+    private String descripcion;
+    private double precio;
+    private String disponibilidad;
+
+    public ServicioAdicional(TipoServicio servicio, String codigo, String nombre,
+                             String descripcion, double precio, String disponibilidad) {
+        this.servicio = servicio;
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.disponibilidad = disponibilidad;
+    }
+
+    public double calcularPrecio() {
+        return precio;
+    }
+
+    @Override
+    public double calcularValor() {
+        return calcularPrecio();
+    }
+
+    public TipoServicio getServicio() {
+        return servicio;
+    }
+
+    public void setServicio(TipoServicio servicio) {
+        this.servicio = servicio;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    @Override
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getDisponibilidad() {
+        return disponibilidad;
+    }
+
+    public void setDisponibilidad(String disponibilidad) {
+        this.disponibilidad = disponibilidad;
+    }
+
+    @Override
+    public String toString() {
+        return codigo + " - " + nombre + " ($" + precio + ")";
+    }
 }
+
